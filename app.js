9a93724f1e4b80a995fff25bcd2c38a94d792500
GemelliAngelo <gemeangelo27@gmail.com>
2025-01-22 11:37:21 +0100

    fix

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const corsOptions = {
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
-//! MIDLEWARES
+//! MIDDLEWARES
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/public", express.static("public"));
