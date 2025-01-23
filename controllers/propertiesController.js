d21149eb4b8fb7815750d5aa3e3eefa8877a6dd5
giuseppemsabino <giuseppemendoza004@gmail.com>
2025-01-23 10:55:06 +0100

    show reviews controller added, and filters(controlli) done

--- controllers/propertiesController.js
+++ controllers/propertiesController.js
@@ -14,7 +14,7 @@ function index(req, res) {
       });
     }
 
-    console.log(results);
+    // console.log(results);
 
     res.json({
       message: "ok",
@@ -46,46 +46,117 @@ function show(req, res) {
     }
     const [property] = results;
 
-    res.json(property);
+    const sqlReviews = `
+        SELECT *
+        FROM reviews
+        WHERE property_id = ?`;
+    connection.query(sqlReviews, [propertyId], (err, results) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({
+          status: "KO",
+          message: "Database query failed",
+        });
+      }
+      if (results.length === 0) {
+        return res.status(404).json({
+          status: "KO",
+          message: "Property not found",
+        });
+      }
+      const reviews = results;
+      console.log(results);
+      
+      res.json({property, reviews});
+    });
   });
-}
-
-function store(req, res){
-    const {title, n_rooms, n_beds, n_bathrooms, square_meters, address, type, email} = req.body;
-    
-   const sql =` INSERT INTO properties (title, n_rooms, n_beds, n_bathrooms, square_meters, address, type, email) 
-   VALUES (?, ?, ?, ?, ?, ?, ?, ?);`
-    connection.query(sql, [title, n_rooms, n_beds, n_bathrooms, square_meters, address, type, email], (err, results) => {
-         if(err){
-              console.log(err);
-              return res.status(500).json({
-                status: "KO",
-                message: "Database query failed"
-              });
-         }
-         res.json({
-              status: "OK",
-              message: "Property created",
-         });
-    }); 
 
 }
 
-function destroy (req, res){
-    const id = parseInt(req.params.id);
+function store(req, res) {
+  const {
+    title,
+    n_rooms,
+    n_beds,
+    n_bathrooms,
+    square_meters,
+    address,
+    type,
+    email,
+  } = req.body;
 
-    const sqlProperty = `
+  if (
+    title.length > 3 &&
+    !isNaN(n_rooms) &&
+    !isNaN(n_beds) &&
+    !isNaN(n_bathrooms) &&
+    !isNaN(square_meters) &&
+    address.length > 3 &&
+    type.length > 3 &&
+    email.includes("@", ".") &&
+    email.length > 3 &&
+    typeof title === "string" &&
+    typeof address === "string" &&
+    typeof email === "string" &&
+    typeof type === "string" &&
+    n_rooms > 0 &&
+    n_rooms % 1 === 0 &&
+    n_beds > 0 &&
+    n_beds % 1 === 0 &&
+    n_bathrooms > 0 &&
+    n_bathrooms % 1 === 0 &&
+    square_meters > 0
+  ) {
+    const sql = ` INSERT INTO properties (title, n_rooms, n_beds, n_bathrooms, square_meters, address, type, email) 
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?);`;
+    connection.query(
+      sql,
+      [
+        title,
+        n_rooms,
+        n_beds,
+        n_bathrooms,
+        square_meters,
+        address,
+        type,
+        email,
+      ],
+      (err) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({
+            status: "KO",
+            message: "Database query failed",
+          });
+        }
+        return res.status(200).json({
+          status: "OK",
+          message: "Property created",
+        });
+      }
+    );
+  } else {
+    return res.status(400).json({
+      status: "KO",
+      message: "Invalid data",
+    });
+  }
+}
+
+function destroy(req, res) {
+  const id = parseInt(req.params.id);
+
+  const sqlProperty = `
     DELETE FROM properties
     WHERE id = ?
     `;
-    connection.query(sqlProperty, [id], (err)=>{
+  connection.query(sqlProperty, [id], (err) => {
     if (err) return res.status(500).json({ error: "Database Query Failed" });
-
-    })
-    res.json({
-        status: "OK",
-        message: "Property deleted",
-   });
+  });
+  res.json({
+    status: "OK",
+    message: "Property deleted",
+  });
 }
 
-module.exports = { index, show, store, destroy};
+module.exports = { index, show, store, destroy };
