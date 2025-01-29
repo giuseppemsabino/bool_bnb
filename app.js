924dd4ef7b56d954b7349734e44e11384f91276c
emazupp <zuppardo.emanuele2001@gmail.com>
2025-01-29 13:01:18 +0100

    sync

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ const { HOST_DOMAIN, HOST_PORT, APP_FRONTEND_URL } = process.env;
 // !CORSE CONFIG
 const corsOptions = {
   origin: APP_FRONTEND_URL,
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  optionsSuccessStatus: 200,
 };
 
-//! MIDDLEWARES
+//! MIDLEWARES
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/public", express.static("public"));
